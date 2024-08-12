3e74e7e9485cc3cf88c1bc738c1f86a74ef8991d
Ramazan <ozturkiramazan@gmail.com>
2024-08-12 15:52:40 +0300

    fix: search page cache

--- src/components/search-page/index.tsx
+++ src/components/search-page/index.tsx
@@ -3,12 +3,19 @@ import SearchResults from "./search-results";
 import FloatingSaveButton from "./floating-save-button";
 import { getRandomElementsFromArray } from "@/lib/utils";
 import { searchSuggessionsArray } from "@/lib/config";
+import { cookies } from "next/headers";
 
 const SearchPage = () => {
   const searchSuggestions = getRandomElementsFromArray(
     searchSuggessionsArray,
     3,
   );
+
+  // Disable caching
+  // https://github.com/vercel/next.js/discussions/50045#discussioncomment-7218266
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _cookies = cookies();
 
   return (
     <>
